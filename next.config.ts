85658be1eb21aa8af78798ba0d3472f17fc17077
Anshu78780 <anshuai.2004@gmail.com>
2025-06-12 19:49:11 +0530

    removes external scrpt

--- next.config.ts
+++ next.config.ts
@@ -29,31 +29,31 @@ const nextConfig: NextConfig = {
     ],
   },
   // Add security headers
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.google.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; connect-src 'self' https:; font-src 'self' data:; object-src 'none'; media-src 'self' https:; frame-src 'self' https://accounts.google.com;",
-          },
-        ],
-      },
-    ];
-  },
+//   async headers() {
+//     return [
+//       {
+//         source: '/(.*)',
+//         headers: [
+//           {
+//             key: 'X-Frame-Options',
+//             value: 'DENY',
+//           },
+//           {
+//             key: 'X-Content-Type-Options',
+//             value: 'nosniff',
+//           },
+//           {
+//             key: 'Referrer-Policy',
+//             value: 'origin-when-cross-origin',
+//           },
+//           {
+//             key: 'Content-Security-Policy',
+//             value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.google.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; connect-src 'self' https:; font-src 'self' data:; object-src 'none'; media-src 'self' https:; frame-src 'self' https://accounts.google.com;",
+//           },
+//         ],
+//       },
+//     ];
+//   },
 };
 
 export default nextConfig;
